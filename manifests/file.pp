3007875d491ede4f73e84a0729b4e8dabf510285
dazole <david@damnetwork.net>
2014-05-29 14:48:53 -0700

    got my vim sytax right...finally.

--- manifests/file.pp
+++ manifests/file.pp
@@ -91,7 +91,7 @@ define staging::file (
       else         { $command = $http_get        }
       exec { $target_file:
         command   => $command,
-	unless    => $get_unless,
+        unless    => $get_unless,
       }
     }
     /^https:\/\//: {
@@ -100,7 +100,7 @@ define staging::file (
       else               { $command = $http_get        }
       exec { $target_file:
         command => $command,
-	unless  => $get_unless,
+        unless  => $get_unless,
       }
     }
     /^ftp:\/\//: {
@@ -108,7 +108,7 @@ define staging::file (
       else               { $command = $ftp_get        }
       exec { $target_file:
         command     => $command,
-	unless      => $get_unless,
+        unless      => $get_unless,
       }
     }
     default: {
