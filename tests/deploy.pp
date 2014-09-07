9456230e06928e80af46a696e68effb005036e3e
Thomas Weißschuh <thomas@t-8ch.de>
2014-09-07 16:43:01 +0000

    add staging::verify

--- tests/deploy.pp
+++ tests/deploy.pp
@@ -1,4 +1,5 @@
 staging::deploy { 'sample.tar.gz':
-  source => 'puppet:///modules/staging/sample.tar.gz',
-  target => '/usr/local',
+  source   => 'puppet:///modules/staging/sample.tar.gz',
+  target   => '/usr/local',
+  checksum => 'e4ad1353b2becccbc9ff49d10b8980f17eadce57',
 }
---
+++ tests/verify.pp
@@ -0,0 +1,3 @@
+staging::verify { 'sample.tar.gz':
+  checksum => 'e4ad1353b2becccbc9ff49d10b8980f17eadce57',
+}
