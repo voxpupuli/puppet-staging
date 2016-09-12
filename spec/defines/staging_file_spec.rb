e9d3e07a36a9a82a2f349badab3408ec05d4e231
Josh Samuelson <js@puppet.com>
2016-09-12 10:56:51 -0700

    more skyle fixes for rubocop

--- spec/defines/staging_file_spec.rb
+++ spec/defines/staging_file_spec.rb
@@ -63,22 +63,24 @@ describe 'staging::file', type: :define do
                                                               cwd: '/opt/staging/',
                                                               creates: '/opt/staging//sample.tar.gz',
                                                               logoutput: 'on_failure')
-      should contain_file('/opt/staging//sample.tar.gz').with( {
-        :owner => nil,
-        :group => nil,
-        :mode  => nil,
-      })
+      should contain_file('/opt/staging//sample.tar.gz').with({
+                                                               owner: nil,
+                                                               group: nil,
+                                                               mode: nil
+                                                              })
     end
   end
 
   describe 'when deploying via http with file parameters' do
     let(:title) { 'sample.tar.gz' }
-    let(:params) { { 
-      source: 'http://webserver/sample.tar.gz',
-      owner: 'root',
-      group: 'root',
-      mode: '0644',
-    } }
+    let(:params) do 
+      { 
+        source: 'http://webserver/sample.tar.gz',
+        owner: 'root',
+        group: 'root',
+        mode: '0644'
+      } 
+    end
 
     it do
       should contain_file('/opt/staging')
@@ -88,11 +90,11 @@ describe 'staging::file', type: :define do
                                                               cwd: '/opt/staging/',
                                                               creates: '/opt/staging//sample.tar.gz',
                                                               logoutput: 'on_failure')
-      should contain_file('/opt/staging//sample.tar.gz').with( {
-        :owner => 'root',
-        :group => 'root',
-        :mode  => '0644',
-      })
+      should contain_file('/opt/staging//sample.tar.gz').with({
+                                                               owner: 'root',
+                                                               group: 'root',
+                                                               mode: '0644'
+                                                              })
     end
   end
 
