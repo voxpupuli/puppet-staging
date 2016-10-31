9bbbdc0b867edc0e04cfe2580d5d694095e27a82
Alexander Fisher <alex@linfratech.co.uk>
2016-10-31 12:02:43 +0000

    rubocop: fix RSpec/ImplicitExpect

--- spec/defines/staging_deploy_spec.rb
+++ spec/defines/staging_deploy_spec.rb
@@ -19,13 +19,13 @@ describe 'staging::deploy', type: :define do
     end
 
     it { is_expected.to compile.with_all_deps }
-    it { should contain_file('/opt/staging') }
-    it { should contain_file('/opt/staging/sample.tar.gz') }
+    it { is_expected.to contain_file('/opt/staging') }
+    it { is_expected.to contain_file('/opt/staging/sample.tar.gz') }
     it do
-      should contain_exec('extract sample.tar.gz').with(command: 'tar xzf /opt/staging/sample.tar.gz',
-                                                        path: '/usr/local/bin:/usr/bin:/bin',
-                                                        cwd: '/usr/local',
-                                                        creates: '/usr/local/sample')
+      is_expected.to contain_exec('extract sample.tar.gz').with(command: 'tar xzf /opt/staging/sample.tar.gz',
+                                                                path: '/usr/local/bin:/usr/bin:/bin',
+                                                                cwd: '/usr/local',
+                                                                creates: '/usr/local/sample')
     end
   end
 
@@ -38,13 +38,13 @@ describe 'staging::deploy', type: :define do
     end
 
     it { is_expected.to compile.with_all_deps }
-    it { should contain_file('/opt/staging') }
-    it { should contain_file('/opt/staging/sample.tar.gz') }
+    it { is_expected.to contain_file('/opt/staging') }
+    it { is_expected.to contain_file('/opt/staging/sample.tar.gz') }
     it do
-      should contain_exec('extract sample.tar.gz').with(command: 'tar xzf /opt/staging/sample.tar.gz',
-                                                        path: '/usr/local/bin:/usr/bin:/bin',
-                                                        cwd: '/usr/local',
-                                                        creates: '/usr/local/sample')
+      is_expected.to contain_exec('extract sample.tar.gz').with(command: 'tar xzf /opt/staging/sample.tar.gz',
+                                                                path: '/usr/local/bin:/usr/bin:/bin',
+                                                                cwd: '/usr/local',
+                                                                creates: '/usr/local/sample')
     end
   end
 
@@ -58,7 +58,7 @@ describe 'staging::deploy', type: :define do
 
     it do
       expect do
-        should contain_exec('extract sample.tar.gz')
+        is_expected.to contain_exec('extract sample.tar.gz')
       end.to raise_error(Puppet::Error, %r{do not recognize source})
     end
   end
@@ -74,13 +74,13 @@ describe 'staging::deploy', type: :define do
     end
 
     it { is_expected.to compile.with_all_deps }
-    it { should contain_file('/opt/staging') }
-    it { should contain_file('/opt/staging/sample.tar.gz') }
+    it { is_expected.to contain_file('/opt/staging') }
+    it { is_expected.to contain_file('/opt/staging/sample.tar.gz') }
     it do
-      should contain_exec('extract sample.tar.gz').with(command: 'tar xzf /opt/staging/sample.tar.gz --strip=1',
-                                                        path: '/usr/local/bin:/usr/bin:/bin',
-                                                        cwd: '/usr/local',
-                                                        creates: '/usr/local/sample')
+      is_expected.to contain_exec('extract sample.tar.gz').with(command: 'tar xzf /opt/staging/sample.tar.gz --strip=1',
+                                                                path: '/usr/local/bin:/usr/bin:/bin',
+                                                                cwd: '/usr/local',
+                                                                creates: '/usr/local/sample')
     end
   end
 
@@ -92,13 +92,13 @@ describe 'staging::deploy', type: :define do
         unzip_opts: '-o -f' }
     end
     it { is_expected.to compile.with_all_deps }
-    it { should contain_file('/opt/staging') }
-    it { should contain_file('/opt/staging/sample.zip') }
+    it { is_expected.to contain_file('/opt/staging') }
+    it { is_expected.to contain_file('/opt/staging/sample.zip') }
     it do
-      should contain_exec('extract sample.zip').with(command: 'unzip -o -f /opt/staging/sample.zip',
-                                                     path: '/usr/local/bin:/usr/bin:/bin',
-                                                     cwd: '/usr/local',
-                                                     creates: '/usr/local/sample')
+      is_expected.to contain_exec('extract sample.zip').with(command: 'unzip -o -f /opt/staging/sample.zip',
+                                                             path: '/usr/local/bin:/usr/bin:/bin',
+                                                             cwd: '/usr/local',
+                                                             creates: '/usr/local/sample')
     end
   end
 end
