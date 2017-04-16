98d2767154bebd817df3cc257eabddac0f2b67c5
Tim Meusel <tim@bastelfreak.de>
2017-04-16 09:50:51 +0200

    rubocop: fix Style/IndentHeredoc

--- lib/puppet/parser/functions/staging_parse.rb
+++ lib/puppet/parser/functions/staging_parse.rb
@@ -2,16 +2,16 @@ require 'uri'
 
 module Puppet::Parser::Functions
   newfunction(:staging_parse, type: :rvalue, doc: <<-EOS
-Parse filepath to retrieve information about the file.
-    EOS
+  Parse filepath to retrieve information about the file.
+  EOS
              ) do |arguments|
     if arguments.empty? || arguments.size > 3
       raise(Puppet::ParseError, 'staging_parse(): Wrong number of arguments ' \
         "given (#{arguments.size} for 1, 2, 3)")
     end
 
-    source    = arguments[0]
-    path      = URI.parse(source.tr('\\', '/')).path
+    source = arguments[0]
+    path   = URI.parse(source.tr('\\', '/')).path
 
     if path.nil?
       raise Puppet::ParseError, "staging_parse(): #{source.inspect} has no URI " \
